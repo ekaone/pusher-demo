cccab894b79077cde381af565a71dab5f6930217
ekaone <ekaone3033@gmail.com>
2021-02-21 11:05:52 +0700

    add .env

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const Pusher = require("pusher");
 
 const pusher = new Pusher({
-  appId: "1159504",
-  key: "b40ccf3305c1a0013d14",
-  secret: "58b83da932c06384bf3b",
-  cluster: "ap1",
+  appId: "",
+  key: "",
+  secret: "",
+  cluster: "",
   useTLS: true,
 });
 
