39a0edcf73ec9c15a50121465aa61e524a53ba37
ekaone <ekaone3033@gmail.com>
2021-03-04 11:47:52 +0700

    add interval data

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const Pusher = require("pusher");
 
 const pusher = new Pusher({
-  appId: "",
-  key: "",
-  secret: "",
-  cluster: "",
+  appId: "1159504",
+  key: "b40ccf3305c1a0013d14",
+  secret: "58b83da932c06384bf3b",
+  cluster: "ap1",
   useTLS: true,
 });
 
-pusher.trigger("my-channel", "my-event", {
-  message: "hello pusher",
-});
+setInterval(() => {
+  pusher
+    .trigger("my-channel", "my-event", {
+      message: Math.random(),
+    })
+    .then(console.log)
+    .catch((e) => console.log(e));
+}, 7000);
